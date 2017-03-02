dd6dba8037462034e7216db52aed11510a246c44
guilhem.heinrich <guilhem.heinrich@gmail.com>
2017-03-02 14:22:14 +0100

    added Calendar class

---
+++ Calendar.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <functional>
+
+#include "room.h"
+#include "schedule.h"
+struct Slot
+{
+	Room &room;
+	Schedule &schedule;
+
+	Slot *next = nullptr;
+};
+
+class Calendar
+{
+public:
+	Calendar();
+	Calendar(std::vector<Slot>);
+	~Calendar();
+
+
+
+private:
+	std::map<Room, std::vector<std::reference_wrapper<Slot> >> _slotsByRoom;
+	std::map<Schedule, std::vector<std::reference_wrapper<Slot> >> _slotsBySchedule;
+	std::vector<Slot> _allSlots;
+};
+
